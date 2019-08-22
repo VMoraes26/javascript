f07e15c11b81718d1c4ea1d4a8b25526023446c5
VINICIUS DE MORAES CARVALHO <21738@unifacef.edu.br>
2019-08-21 21:45:57 -0300

    Exemplo vetor de objetos v0.4

--- vetObjeto.js
+++ vetObjeto.js
@@ -1,3 +1,15 @@
 function funcao() {
+    var vet = [];
+    
+    for (var i = 0; i < 5; i++) {
+        var objeto = {};
+        //Coloca os dados no objeto
+        objeto.codigo = Number(prompt("Código"));
+        objeto.nome = prompt("Nome");
+        objeto.qtde = Number(prompt("Qtde"));
+        objeto.preco = Number(prompt("Preço"));
 
+        //Coloca o objeto no vetor
+        vet.push(objeto);
+    }
 }
