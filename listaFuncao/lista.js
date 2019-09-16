ca530838abee1f88fe0abc3f2b657ce9a5ad5d54
VINICIUS DE MORAES CARVALHO <21738@unifacef.edu.br>
2019-09-16 20:36:53 -0300

    Ex 15 ao 19

--- listaFuncao/lista.js
+++ listaFuncao/lista.js
@@ -31,6 +31,46 @@ function lista() {
                 let num2 = Number(prompt("Entre com um numero inteiro"))
                 ex5(num2)
                 break;
+
+
+            case 15:
+                let numeros = [];
+                for (var i = 0; i < 15; i++) {
+                    numeros.push(Number(prompt("Entre com números inteiros")))
+                }
+                ex15(numeros)
+                break;
+            case 16:
+                let numerosReais = [];
+                for (var i = 0; i < 20; i++) {
+                    numerosReais.push(Number(prompt("Entre com números reais (com ponto)")))
+                }
+                ex16(numerosReais)
+                break;
+            case 17:
+                let numerosInteiros = [];
+                for (var i = 0; i < 25; i++) {
+                    numerosInteiros.push(Number(prompt("Entre com números inteiros negativos ou positivos")))
+                }
+                ex17(numerosInteiros)
+                break;
+            case 18:
+                ex18()
+                break;
+            case 19:
+                let vet1 = [];
+                let vet2 = [];
+                for (var i = 0; i < 10; i++) {
+                    vet1.push(Number(prompt("Entre com um número inteiros para o primerio vetor")))
+                }
+                for (var i = 0; i < 10; i++) {
+                    vet2.push(Number(prompt("Entre com um número inteiros para o segundo vetor")))
+                }
+                ex19(vet1, vet2)
+                break;
+            case 20:
+                ex20(num2)
+                break;
             default:
                 alert("Exercicio não existe")
                 break;
@@ -75,4 +115,71 @@ function ex5(num) {
     } else {
         alert("O número é positivo")
     }
+}
+
+
+
+function ex15(numeros) {
+    var qtd = 0
+    numeros.forEach(numero => {
+        if (numero % 2 == 0) {
+            qtd++
+        }
+    })
+    alert(`Quantidade de números pares: ${qtd}`)
+}
+
+function ex16(numerosReais) {
+    var soma = 0
+    numerosReais.forEach(numero => {
+        soma += numero
+    })
+    alert(`Soma dos números: ${soma}`)
+}
+
+function ex17(numerosInteiros) {
+    numerosInteiros.forEach(numero => {
+        if (numero < 0) {
+            numero = 0
+        }
+    })
+    alert(`Vetor sem números negativos: ${numerosInteiros}`)
+}
+
+function ex18() {
+    let num = 100
+    let count = 0
+    var vet = []
+    while (count < 10) {
+        num++
+        let divisao = false
+
+        for (var i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                divisao = true
+            }
+        }
+
+        if (!divisao) {
+            vet.push(num)
+            count++
+        }
+    }
+    alert("10 números primos\n" + vet)
+
+}
+
+function ex19(vet1, vet2) {
+    var vetIntersec = []
+    for (var i = 0; i < 10; i++) {
+        for (var j = 0; j < 10; j++) {
+            if (vet1[i] == vet2[j]) {
+                vetIntersec.push(vet1[i])
+            }
+        }
+    }
+    alert("Vetor intersecção: " + vetIntersec)
+}
+
+function ex20(num) {
 }
